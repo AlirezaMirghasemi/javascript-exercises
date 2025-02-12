2f5be8c4ef28c2ac651d599369ae581e7fd9f8d4
AlirezaMirghasemi <alireza.mirghasemi@gmail.com>
2025-02-12 13:07:46 +0330

    calculateAverage refactor done

--- advanced/calculateAverage.js
+++ advanced/calculateAverage.js
@@ -9,8 +9,9 @@
  * calculateAverage([1, 2, 3, 4]) should return 2.5.
  * calculateAverage([10, 20, 30]) should return 20.
  */
-let calculateAverage=(arr)=>{
-  let sum=  arr.reduce((sum,cell)=>sum+cell,0);
-  return sum/arr.length;
-}
+let calculateAverage = (arr) => {
+  if (arr.length === 0) return 0;
+  let sum = arr.reduce((sum, cell) => sum + cell, 0);
+  return sum / arr.length;
+};
 module.exports = calculateAverage;
