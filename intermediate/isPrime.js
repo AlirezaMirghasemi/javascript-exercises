3c622473346a4d8f596231bb317732dd3d2ad1c2
AlirezaMirghasemi <alireza.mirghasemi@gmail.com>
2025-02-12 13:05:39 +0330

    isPrime refactor done

--- intermediate/isPrime.js
+++ intermediate/isPrime.js
@@ -13,8 +13,10 @@
 // Write your solution here
 let isPrime=(num)=>{
     if(num<=1) return false;
-    for (let index = 2; index < num; index++) {
-        if((num%index==0)) return false;
+    if (num === 2) return true;
+    if (num % 2 === 0) return false;
+    for (let i = 3; i < num; i+=2) {
+        if((num%i==0)) return false;
     }
     return true;
 }
